31592e303f70ceaf230b6d4d70d7394f6bd789d8
Matheus Pereira <matheus@stargrid.pro>
2023-04-07 17:40:15 -0300

    component search e logo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,73 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import Logo from "../../components/Logo";
 
 export default function Search() {
+  const [inputValue, setInputValue] = useState("");
+
+  function handleSearch() {
+    console.log(inputValue);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Página Home!!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Logo />
+      <Text style={styles.title}>Encontre a receita</Text>
+      <Text style={styles.title}>que combina com você</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          placeholder="Digite o nome da comida..."
+          style={styles.input}
+          value={inputValue}
+          onChangeText={(text) => setInputValue(text)}
+        />
+        <TouchableOpacity onPress={handleSearch}>
+          <Ionicons name="search" size={28} color="#4CBE6C" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "black",
+    flex: 1,
+    backgroundColor: "#F3F9FF",
+    paddingTop: 36,
+    paddingStart: 14,
+    paddingEnd: 14,
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: "bold",
+    color: "#0E0E0E",
+  },
+  form: {
+    backgroundColor: "#FFF",
+    width: "100%",
+    borderRadius: 8,
+    marginTop: 16,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: "#ECECEC",
+    paddingLeft: 8,
+    paddingRight: 8,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  input: {
+    width: "90%",
+    maxWidth: "90%",
+    height: 54,
   },
 });
